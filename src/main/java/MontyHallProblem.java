7de8f0b75db94bbfbdd2d0361c008c89da379ea9
alphafoobar <james@ergo.kiwi>
2017-06-27 14:23:15 +0100

    Initial commit with maven, checkstyle and findbugs

--- src/main/java/MontyHallProblem.java
+++ src/main/java/MontyHallProblem.java
@@ -6,7 +6,7 @@ public class MontyHallProblem {
 
   private static final long numberOfTrials = 1000000;
   private static final long numberOfDoors = 3;
-  
+
   private static final String CHOICE_GOAT = "Goat";
   private static final String CHOICE_CAR = "Car";
   private static long numberofTrailsCounter = 0;
@@ -19,14 +19,14 @@ public class MontyHallProblem {
       for (long counter=1; counter <= numberOfTrials; counter++){
         long currentNumberOfDoorsToChooseFrom = numberOfDoors;
         setupDoorsAndPrizes();
-        long contestantsInitialPick = getRandomNumberofaDoor(numberOfDoors);    
+        long contestantsInitialPick = getRandomNumberofaDoor(numberOfDoors);
 		Long mapKeyForCcontestantsInitialPick = new Long(contestantsInitialPick);
-       
+
         if (doorsAndPrizes.containsKey(mapKeyForCcontestantsInitialPick)) {
-            String contestantsInitialPrize = (String)doorsAndPrizes.get(mapKeyForCcontestantsInitialPick); 
+            String contestantsInitialPrize = (String)doorsAndPrizes.get(mapKeyForCcontestantsInitialPick);
             doorsAndPrizes.remove(mapKeyForCcontestantsInitialPick);
         }
-        
+
           currentNumberOfDoorsToChooseFrom--;
           long revealedDoor = getRandomNumberofaDoor(currentNumberOfDoorsToChooseFrom);
 
@@ -45,22 +45,22 @@ public class MontyHallProblem {
           numberofTrailsCounter++;
 
           if (finalPrize.equalsIgnoreCase(CHOICE_CAR)) {
-              numberofWinsCounter++;            
+              numberofWinsCounter++;
           }
       }
-      
+
       long endTime = (System.currentTimeMillis());
       System.out.println("Results: " + numberofTrailsCounter + " trials and " + numberofWinsCounter + " winners that switched");
     }
 
 
   private static String whatDidContestantWin(long doorContestantSwitchedTo) {
-    String returnValue = null;  
-    
+    String returnValue = null;
+
     long doorCounter = 1;
     Long key = null;
-    String value = null; 
-    
+    String value = null;
+
     Iterator keyValuePairs = doorsAndPrizes.entrySet().iterator();
         while (keyValuePairs.hasNext()) {
           Map.Entry entry = (Map.Entry) keyValuePairs.next();
@@ -70,56 +70,56 @@ public class MontyHallProblem {
             returnValue = value;
           break;
       }
-          doorCounter++;          
+          doorCounter++;
         }
 
         return returnValue;
   }
-    
+
   private static boolean revealDoor(long aRevealedDoor) {
     boolean returnValue = false;
     long numberOfDoors = doorsAndPrizes.size();
     long revealCounter=1;
     Long key = null;
-    String value = null; 
-    
+    String value = null;
+
     Iterator keyValuePairs = doorsAndPrizes.entrySet().iterator();
         while (keyValuePairs.hasNext()) {
           Map.Entry entry = (Map.Entry) keyValuePairs.next();
-      
+
           if (aRevealedDoor == revealCounter) {
             key = (Long)entry.getKey();  //  key is not used
             value = (String)entry.getValue();
-          
+
           if (value.equalsIgnoreCase(CHOICE_GOAT)) {
             returnValue = true;
-                doorsAndPrizes.remove(key);           
+                doorsAndPrizes.remove(key);
           } else {
             returnValue = false;
           }
-          break;          
+          break;
       }
-          revealCounter++;          
+          revealCounter++;
         }
     return returnValue;
   }
-  
+
   private static long getRandomNumberofaDoor(long upperRangeofRandomNumber) {
     long randomIntofaDoor = 1 + (int)(Math.random() * upperRangeofRandomNumber);
     return randomIntofaDoor;
   }
-  
+
   private static void setupDoorsAndPrizes() {
     doorsAndPrizes = new LinkedHashMap();
     long randomInt = getRandomNumberofaDoor(numberOfDoors);
 
     for (int counter=1; counter <= numberOfDoors ;counter++){
         if (randomInt == counter) {
-            doorsAndPrizes.put(new Long(counter), CHOICE_CAR);        
+            doorsAndPrizes.put(new Long(counter), CHOICE_CAR);
         } else {
             doorsAndPrizes.put(new Long(counter), CHOICE_GOAT);
         }
     }
    }
-   
+
 }
